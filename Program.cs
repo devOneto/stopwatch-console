159575836bc47caf7b804857f2ba60afe8775105
Neto (João Araújo) <bipnerds@gmail.com>
2021-04-28 09:38:44 -0300

    Simple Stopwatch

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stopwatch.");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = segundos");
+            Console.WriteLine("M = minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Oque desejafazer?");
+            
+            string data = Console.ReadLine().ToLower();
+            char type = Convert.ToChar(data.Substring(data.Length - 1,1));
+            Console.WriteLine(type);
+            int time = Convert.ToInt32(data.Substring(0,data.Length - 1));
+            Console.WriteLine(time);
+            int multiplier = 60;
+
+            switch(type){
+                case 's': Start(time); break;
+                case 'm': Start(time*multiplier); break;
+                default: Menu(); break;
+            }
+
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+            while ( currentTime != time )
+            {
+                Console.Clear();
+                currentTime ++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stop!");
+
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Stopwatch in C#
+
+### Some Concepts to Remember...
+
+1. Switch case Structure
+2. String's Substring method
+3. Data Convertions
+4. Thread Sleep
